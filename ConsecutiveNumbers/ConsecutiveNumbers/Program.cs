2f6b35d151d597f35ce6077a5f2ec9d88dc92e73
ubeffiong <ubokobongokon@gmail.com>
2022-11-06 13:49:12 +0100

    Consecutive numbers

---
+++ ConsecutiveNumbers/ConsecutiveNumbers/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsecutiveNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input1 = 6;
+            int[] input2 = { 1, 3, 6, 2, 4, 5};
+            int[] input3 = { -1, 3, 6, 9, 4, 5 };
+
+            Console.WriteLine( "Input 2");
+            if (isConsecutive(input2, input1))
+                    Console.WriteLine(1);
+            else
+                    Console.WriteLine(0);
+            Console.WriteLine("Input 3");
+            if (isConsecutive(input3, input1))
+                Console.WriteLine(1);
+            else
+                Console.WriteLine(0);
+        }
+        private static bool isConsecutive(int[] input2, int input1)
+        {
+            Array.Sort(input2);
+            // Check if all element in the array are distinct
+            //traverse the array and checks if each element appears only once
+            for (int i = 0; i < input2.Length - 1; i++)
+            {
+                if (input2[i] == input2[i + 1])
+                    break;
+            }
+
+            //check if all element present in the set are consecutive
+            //consecutive elements is 1
+            //this will check if there is difference b/w consecutive elements in the sorted array = 1
+            for (int i = 1; i < input1; i++)
+                if (input2[i] - input2[i - 1] != 1)
+                    return false;
+            return true;
+        }
+        private static bool isConsecutive2(int[] input3, int input)
+        {
+            if (input <=1)
+                return true;
+            int min = input3[0];
+            int max = input3[0];
+
+            //compute the minimum and maximum element in the array
+            //difference between maximum and minimum element should be input - 1
+            for (int i = 1; i <input; i++)
+            {
+                if (input3[i] < min)
+                    min = input3[i];
+                if (input3[i] > max)
+                    max = input3[i];
+            }
+            if (min - max != input - 1)
+            {
+                return false;
+            }
+            // Check if all element in the array are distinct
+            //traverse the array and checks if each element appears only once
+            var inputList = new HashSet<int>();
+            foreach (var v in input3)
+            {
+                if (inputList.Contains(v))
+                    return false;
+                else
+                    inputList.Add(v);
+            }
+            return true;
+
+        }
+    }
+
+}
--- RearrrangeStringAnagram/RearrrangeStringAnagram/Program.cs
+++ RearrrangeStringAnagram/RearrrangeStringAnagram/Program.cs
@@ -10,43 +10,111 @@ namespace RearrrangeStringAnagram
             string firstWord = "zabk"; string secondWord = "kbza";
             char[] aa = firstWord.ToLower().ToCharArray();
             char[] bb = secondWord.ToLower().ToCharArray();
+            int[] numb = { 1, 2, 7, 9, 6 };
+            int lastItem = numb[^1];
+            Console.WriteLine(lastItem);
+
             //Array.Sort(aa); Array.Sort(bb);
 
             //Sorting without using function
-            char tempA;
-            char tempB;
-            for (int i = 0; i < aa.Length-1; i++ )
+            //char tempA;
+            //char tempB;
+            //for (int i = 0; i < aa.Length-1; i++ )
+            //{
+            //    for (int j = i + 1; j <= aa.Length-1; j++)
+            //    {
+            //        if (aa[i] > aa[j])
+            //        {
+            //            tempA = aa[i];
+            //            aa[i] = aa[j];
+            //            aa[j] = tempA;
+            //        }
+            //    }
+            //}
+            //for (int i = 0; i < bb.Length - 1; i++)
+            //{
+            //    for (int j = i + 1; j <= bb.Length - 1; j++)
+            //    {
+            //        if (bb[i] > bb[j])
+            //        {
+            //            tempB = bb[i];
+            //            bb[i] = bb[j];
+            //            bb[j] = tempB;
+            //        }
+            //    }
+            //}
+
+            //string sortedAA = new string(aa);      
+            //Console.WriteLine(sortedAA);
+            //string sortedBB = new string(bb);
+            //Console.WriteLine(sortedBB);
+            //if (sortedAA == sortedBB)
+            //    Console.WriteLine("Passed" + "  - it is Anagram");
+            //else
+            //    Console.WriteLine("Failed");
+
+            CountSort(aa,bb);
+            string newAA = "";
+            string newBB = "";
+            int num = 100_000_000;
+            
+            for (int i = 0; i < aa.Length; i++)
             {
-                for (int j = i + 1; j <= aa.Length-1; j++)
-                {
-                    if (aa[i] > aa[j])
-                    {
-                        tempA = aa[i];
-                        aa[i] = aa[j];
-                        aa[j] = tempA;
-                    }
-                }
+                
+                newAA = char.ToString(aa[i]);
+                Console.WriteLine(newAA);
             }
-            for (int i = 0; i < bb.Length - 1; i++)
+            Console.WriteLine(newAA + " checking");
+            for (int i = 0; i < bb.Length; i++)
             {
-                for (int j = i + 1; j <= bb.Length - 1; j++)
-                {
-                    if (bb[i] > bb[j])
-                    {
-                        tempB = bb[i];
-                        bb[i] = bb[j];
-                        bb[j] = tempB;
-                    }
-                }
+                newBB = char.ToString(bb[i]);
+                Console.WriteLine(newBB);
+                Console.WriteLine(num);
+                
             }
-            string sortedAA = new string(aa);
-            Console.WriteLine(sortedAA);
-            string sortedBB = new string(bb);
-            Console.WriteLine(sortedBB);
-            if (sortedAA == sortedBB)
+           
+            if (newAA == newBB)
                 Console.WriteLine("Passed" + "  - it is Anagram");
             else
                 Console.WriteLine("Failed");
+        }
+        static void CountSort (char[]aa, char[]bb)
+        {
+          
+            char[] aa2 = new char [aa.Length];
+            int[] count = new int [256];
+            for (int i = 0; i < 256; i++)
+                count[i] = 0;
+            for (int i = 0; i < aa.Length; i++)
+                ++count[aa[i]];
+            for (int i = 1; i <= 255; ++i)
+                count[i] += count[i - 1];
+            for (int i =  aa.Length - 1; i >= 0; i--)
+            {
+                aa2[count[aa[i]] - 1] = aa[i];
+                    --count[aa[i]];
+            }
+            for (int i = 0; i < aa.Length; i++)
+                aa[i] = aa2 [i];
+            
+    
+
+            char[] bb2 = new char[bb.Length];
+            int[] countB = new int[256];
+            for (int i = 0; i < 256; i++)
+                countB[i] = 0;
+            for (int i = 0; i < bb.Length; i++)
+                ++countB[bb[i]];
+            for (int i = 1; i <= 255; ++i)
+                countB[i] += countB[i - 1];
+            for (int i = bb.Length - 1; i >= 0; i--)
+            {
+                bb2[countB[bb[i]] - 1] = bb[i];
+                --countB[bb[i]];
+            }
+            for (int i = 0; i < bb.Length; i++)
+                bb[i] = bb2[i];
+
         }
     }
 }
