da39c1624aa9425366dbbae8707643afb6f3abaa
ubeffiong <ubokobongokon@gmail.com>
2023-05-07 19:47:56 +0100

    Converting Integer to Roman numerals

---
+++ DNAsequence/DNAsequence/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DNAsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dna = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+            var repeatedDNA = FindRepeatedDnaSequences(dna);
+            foreach (var sequence in repeatedDNA)
+            {
+                Console.WriteLine(sequence);
+            }
+        }
+        public static IList<string> FindRepeatedDnaSequences(string dnaSequence)
+        {
+            HashSet<string> SeenSequence = new HashSet<string>();
+            HashSet<string> RepeatedSequence = new HashSet<string>();
+            List<string> repeatedSequencesList = new List<string>();
+
+            for (int i = 0; i <= dnaSequence.Length - 10; i++)
+            {
+                string subSequence = dnaSequence.Substring(i, 10);
+                if (SeenSequence.Contains(subSequence))
+                {
+                    RepeatedSequence.Add(subSequence);
+                }
+                else
+                {
+                    SeenSequence.Add(subSequence);
+                }
+            }
+            foreach (string subSequence in RepeatedSequence)
+            {
+                repeatedSequencesList.Add(subSequence);
+            }
+            return repeatedSequencesList;
+        }
+    }
+}
---
+++ IntergerToRoman/IntergerToRoman/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IntergerToRoman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 3;
+            Console.WriteLine(IntToRoman(num));
+        }
+        public static string IntToRoman(int num)
+        {
+            if (num <= 0 || num >= 3999)
+            {
+                //Do something
+            }
+            string[] thousand = { "", "M", "MM", "MMM" };
+            string[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            string[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            string[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+            string romanNumerals = thousand[num/1000] + hundreds[(num % 1000)/100] + tens[(num % 100)/10] + ones[num%10];
+            return romanNumerals;
+        }
+
+    }
+}
